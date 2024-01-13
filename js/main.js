6ded2c99566447252261ff23a41ca63d8b6d4a90
unknown <iryna.sviatetska@gmail.com>
2024-01-13 12:57:46 +0200

    test

--- js/main.js
+++ js/main.js
@@ -71,12 +71,12 @@ $(document).ready(function() {
 
     attrFancy();
 
-    // other
-    // $(".modal").on("show", function () {
-    //     $("body").addClass("modal-open");
-    // }).on("hidden", function () {
-    //     $("body").removeClass("modal-open")
-    // });
+    //other
+    $(".modal").on("show", function () {
+        $("body").addClass("modal-open");
+    }).on("hidden", function () {
+        $("body").removeClass("modal-open")
+    });
 
 });
 
